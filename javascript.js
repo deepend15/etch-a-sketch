03624a4881efb08cbcbc989fb83406589e8c3695
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-08-07 12:40:28 -0400

    Add code to create grid

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
-const container = document.querySelector(".container");
+function addSquares(number) {
+    const container = document.querySelector(".container");
+    for (let i = 1; i <= number ** 2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flex = `1 1 calc(100% / ${number})`;
+        container.appendChild(square);
+    }
+}
 
-const square = document.createElement("div");
+addSquares(16);
